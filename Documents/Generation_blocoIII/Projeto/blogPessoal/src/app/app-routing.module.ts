7103c554be9e41486237bd0d8d7bfe1fb146559f
Jezebel1990 <jezebelguedes@gmail.com>
2021-05-31 14:29:44 -0300

    Rotas de entrar e cadastrar criadas

---
+++ Documents/Generation_blocoIII/Projeto/blogPessoal/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { EntrarComponent } from './entrar/entrar.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
+
+const routes: Routes = [
+
+  {path:'', redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path: 'entrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+  title = 'Olá Mundo!';
+}
